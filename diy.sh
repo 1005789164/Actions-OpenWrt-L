240fa2a0edc0badc2d926bf8ad006ceb9083c448
MJ <1005789164@qq.com>
2020-02-10 23:08:46 +0800

    modify and bug fix

--- diy.sh
+++ diy.sh
@@ -7,3 +7,9 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 修改默认主题
+local DEFAULT_THEME="netgear"
+sed '/option mediaurlbase \/.*\//s/static.*/static\/'$DEFAULT_THEME'/' feeds/luci/modules/luci-base/root/etc/config/luci
+
+#
