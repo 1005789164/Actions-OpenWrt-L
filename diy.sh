add6ba479fe99e50abe26f638fc825abea9a41b0
MJ <1005789164@qq.com>
2020-02-15 02:17:25 +0800

    start

--- diy.sh
+++ diy.sh
@@ -9,7 +9,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-DEFAULT_THEME="argon_dark"
+DEFAULT_THEME="bootstrap_blue"
 #sed -i '/option mediaurlbase \/.*\//s/static.*/static\/'$DEFAULT_THEME'/' feeds/luci/modules/luci-base/root/etc/config/luci
 sed -i '2auci set luci.main.mediaurlbase=/luci-static/'$DEFAULT_THEME'' package/default-settings/files/zzz-default-settings
 
