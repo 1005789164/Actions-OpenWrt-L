df2653655e6eb29cab0dd21daf039eba0ad4c44b
MJ <1005789164@qq.com>
2020-02-21 23:48:55 +0800

    start

--- diy.sh
+++ diy.sh
@@ -17,11 +17,16 @@
 curl -fsSL https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/default-settings/files/zzz-default-settings >> ./default-settings
 sed -n 18,28p ./default-settings > ./default-settings-content
 sed -i '/uci commit fstab/r ./default-settings-content' package/default-settings/files/zzz-default-settings
-rm -f default-setting*
+rm -rf default-setting*
 
 # 更新smartdns
-sed -i 's/d6c34658af0d4aac94655a139030c5ec12884754/0d2d08586a34d83fee7d9ad6199eade81ffe0f64/' package/lean/smartdns/Makefile
-sed -i 's/2019.12.15/2020.02.20/' package/lean/smartdns/Makefile
-sed -i 's/PKG_RELEASE:=2/PKG_RELEASE:=3/' package/lean/smartdns/Makefile
+rm -rf package/lean/smartdns package/lean/luci-app-smartdns
+git clone --depth 1 https://github.com/1005789164/openwrt-smartdns.git MJ-dns
+mv MJ-dns/smartdns package/lean/
+mv MJ-dns/luci-app-smartdns package/lean/
+rm -rf MJ-dns
+#sed -i 's/d6c34658af0d4aac94655a139030c5ec12884754/0d2d08586a34d83fee7d9ad6199eade81ffe0f64/' package/lean/smartdns/Makefile
+#sed -i 's/2019.12.15/2020.02.20/' package/lean/smartdns/Makefile
+#sed -i 's/PKG_RELEASE:=2/PKG_RELEASE:=3/' package/lean/smartdns/Makefile
 
 #
