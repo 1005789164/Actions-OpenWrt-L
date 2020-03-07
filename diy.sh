330fdfc579699085c208b398424a48fbf88e22cc
MJ <1005789164@qq.com>
2020-03-07 12:23:05 +0800

    test

--- diy.sh
+++ diy.sh
@@ -9,13 +9,18 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-#DEFAULT_THEME="bootstrap_blue"
+DEFAULT_THEME="bootstrap_mod"
 #sed -i '/option mediaurlbase \/.*\//s/static.*/static\/'$DEFAULT_THEME'/' feeds/luci/modules/luci-base/root/etc/config/luci
-#sed -i '2auci set luci.main.mediaurlbase=/luci-static/'$DEFAULT_THEME'' package/default-settings/files/zzz-default-settings
+sed -i '/mediaurlbase/d' package/default-settings/files/zzz-default-settings
+sed -i '3auci set luci.main.mediaurlbase=/luci-static/'$DEFAULT_THEME package/default-settings/files/zzz-default-settings
+grep -rin "url('admin/translations'" feeds/lienol/lienol/ | while read i
+do
+[ -f "$(echo $i | cut -d: -f1)" ] && sed -i "/url('admin\/translations'/d" "$(echo $i | cut -d: -f1)"
+done
 
-# 修改默认设置文件(移动菜单里面的项到网络存储)
+# 移动菜单服务里面的项到网络存储
 curl -fsSL https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/default-settings/files/zzz-default-settings >> ./default-settings
-sed -n 18,28p ./default-settings > ./default-settings-content
+sed -n '/nas/p' ./default-settings > ./default-settings-content
 sed -i '/uci commit fstab/r ./default-settings-content' package/default-settings/files/zzz-default-settings
 rm -rf default-setting*
 
@@ -28,5 +33,9 @@ rm -rf MJ-dns
 #sed -i 's/d6c34658af0d4aac94655a139030c5ec12884754/0d2d08586a34d83fee7d9ad6199eade81ffe0f64/' package/lean/smartdns/Makefile
 #sed -i 's/2019.12.15/2020.02.20/' package/lean/smartdns/Makefile
 #sed -i 's/PKG_RELEASE:=2/PKG_RELEASE:=3/' package/lean/smartdns/Makefile
+
+# 修改默认设置
+sed -i 's/SNAPSHOT/SNAPSHOT-'$(TZ=UTC-8 date +"%Y.%m.%d")'/' package/default-settings/files/zzz-default-settings
+
 
 #
